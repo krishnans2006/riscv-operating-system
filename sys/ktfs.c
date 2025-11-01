c07f8fc13fce4c88e9ec968ebf3e32af85e06a20
sz92 <sz92@illinois.edu>
2025-11-01 17:24:36 -0500

    ktfs open file minor bug fix

--- sys/ktfs.c
+++ sys/ktfs.c
@@ -235,11 +235,11 @@ static int ktfs_open_file(struct ktfs_fs* ktfs, const char* name, struct uio** u
     }
 
     // Go through the root directory to find the file
-    int num_dentries = root_inode->size / KTFS_DENSZ;  // Must be exactly divisible
+    uint32_t num_dentries = root_inode->size / KTFS_DENSZ;  // Must be exactly divisible
 
     struct ktfs_dir_entry* dentry;
     
-    for (int i = 0; i < num_dentries; i++) {
+    for (uint32_t i = 0; i <= num_dentries; i++) {
         // Get nth dentry
         result = ktfs_get_nth_dentry(ktfs, root_inode, i, &dentry);
         if (result != 0) {
