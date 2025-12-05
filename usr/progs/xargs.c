f30268c4ec120b1b6bde47ed44e9cb5e684aaa80
Krishnan Shankar <krishnans2006@gmail.com>
2025-12-05 13:00:54 -0600

    Add template for xargs program

---
+++ usr/progs/xargs.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+#include "string.h"
+#include "syscall.h"
+
+// A program that reads items from STDIN, separated by spaces or newlines, and executes the command
+// specified in its arguments with those items as additional arguments
+
+void main(int argc, char *argv[]) {
+    // Implement after shell
+    return;
+}
